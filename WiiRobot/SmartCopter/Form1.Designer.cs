7340ec85f33812cf37b5b89c62b9b1aa2867e8ea
Марков Алексей <markovalex95@gmail.com>
2014-08-28 17:16:11 +0400

    Robot debugger
    Windows program to debug robot

---
+++ WiiRobot/SmartCopter/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace SmartCopter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Com = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.LogChb = new System.Windows.Forms.CheckBox();
+            this.AutoScrollChb = new System.Windows.Forms.CheckBox();
+            this.logBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.GraphBox = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GraphBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(619, 346);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Com
+            // 
+            this.Com.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.Com_DataReceived);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.LogChb);
+            this.groupBox1.Controls.Add(this.AutoScrollChb);
+            this.groupBox1.Controls.Add(this.logBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(747, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(275, 376);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Log";
+            // 
+            // LogChb
+            // 
+            this.LogChb.AutoSize = true;
+            this.LogChb.Location = new System.Drawing.Point(89, 353);
+            this.LogChb.Name = "LogChb";
+            this.LogChb.Size = new System.Drawing.Size(68, 17);
+            this.LogChb.TabIndex = 13;
+            this.LogChb.Text = "Print Log";
+            this.LogChb.UseVisualStyleBackColor = true;
+            // 
+            // AutoScrollChb
+            // 
+            this.AutoScrollChb.AutoSize = true;
+            this.AutoScrollChb.Checked = true;
+            this.AutoScrollChb.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.AutoScrollChb.Location = new System.Drawing.Point(6, 353);
+            this.AutoScrollChb.Name = "AutoScrollChb";
+            this.AutoScrollChb.Size = new System.Drawing.Size(77, 17);
+            this.AutoScrollChb.TabIndex = 12;
+            this.AutoScrollChb.Text = "Auto Scroll";
+            this.AutoScrollChb.UseVisualStyleBackColor = true;
+            // 
+            // logBox
+            // 
+            this.logBox.Location = new System.Drawing.Point(6, 19);
+            this.logBox.Multiline = true;
+            this.logBox.Name = "logBox";
+            this.logBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.logBox.Size = new System.Drawing.Size(262, 306);
+            this.logBox.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(6, 330);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "0 0 0";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.GraphBox);
+            this.groupBox2.Location = new System.Drawing.Point(13, 6);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(728, 334);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // GraphBox
+            // 
+            this.GraphBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.GraphBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.GraphBox.Location = new System.Drawing.Point(6, 19);
+            this.GraphBox.Name = "GraphBox";
+            this.GraphBox.Size = new System.Drawing.Size(700, 300);
+            this.GraphBox.TabIndex = 6;
+            this.GraphBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1043, 397);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "SmartCopter";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_Closing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.GraphBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox LogChb;
+        private System.Windows.Forms.CheckBox AutoScrollChb;
+        private System.Windows.Forms.TextBox logBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox GraphBox;
+    }
+}
+
---
+++ WiiRobot/SmartCopter/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Drawing.Drawing2D;
+using System.Collections.Generic;
+
+namespace SmartCopter
+{
+    
+    public partial class Form1 : Form
+    {
+
+        //=============== Graph data ==================
+        Bitmap bmp;
+        Graphics g;
+
+        float ScaleY = 1;
+
+        int X, Y, Xo, Yo;
+        int CenterX, CenterY;
+        int Yaw=0;
+        //================ General ======================
+        bool isReceived = false;
+
+
+        SerialPort Com;
+
+        String inS="";
+        //=============== Input options ==================
+        const byte NumOfParam = 3;
+        //================================================
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(GraphBox.Width, GraphBox.Height);
+            g = Graphics.FromImage(bmp);
+            g.Clear(Color.White);
+            GraphBox.Image = bmp;
+
+
+            CenterX  = GraphBox.Width / 2;
+            CenterY  = GraphBox.Height / 2;
+           
+            X = 0;
+            Y = 0;
+            Xo = 0;
+            Yo = 0;
+
+           
+
+            Com.PortName = "COM7";
+            
+        }
+        //***************************************************
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Com.Open();
+            if (Com.IsOpen) { MessageBox.Show("Port is open"); timer1.Enabled = true; } 
+        }
+        //****************************************************
+        private void Form1_Closing(object sender, EventArgs  e)
+        {
+            Com.Close();
+            if (!Com.IsOpen) { timer1.Enabled = false; MessageBox.Show("Port closed"); } 
+        }
+        //*******************************************************
+        private void Com_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+
+          
+
+            inS = Com.ReadLine();
+            inS = inS.Remove(inS.Length - 1);
+
+            if (inS.Length != 0)
+            {
+                if ((inS[0] == 'N') && (inS[inS.Length - 1] == 'E'))
+                {
+                    int[] _buf = new int[NumOfParam];
+
+
+                    String _param = "";
+                    inS = inS.Substring(1, inS.Length - 2);
+
+                    int _j = 0; //Read position
+
+                    for (byte _i = 0; _i < NumOfParam; _i++)
+                    {
+                        while ((inS[_j] != ' ') && (_j < inS.Length))
+                        {
+                            _param += inS[_j];
+                            _j++;
+                        }
+
+
+                        _buf[_i] = Convert.ToInt32(_param);
+
+                        _j++;
+                        _param = "";
+
+                    }
+
+                    X = _buf[0];
+                    Y = _buf[1];
+                    Yaw = _buf[2];
+                    isReceived = true;
+                }
+            }
+        }
+
+        //************************************************
+
+        void DrawGrap()
+        {
+
+            int Scale = 5;
+            g.DrawLine(new Pen(Brushes.Black),CenterX-X*Scale,CenterY-Y*Scale, CenterX-Xo*Scale, CenterY-Yo*Scale );
+            Xo = X;
+            Yo = Y;
+            GraphBox.Image = bmp;
+
+        }
+
+      
+
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (isReceived)
+            {
+                DrawGrap();
+                //----------------------
+               
+                //----------------------
+                if (LogChb.Checked)
+                {
+                    logBox.Text += inS + Environment.NewLine;
+                    if (AutoScrollChb.Checked)
+                    {
+                        logBox.SelectionStart = logBox.Text.Length - 1;
+
+                    }
+                    logBox.ScrollToCaret();
+                }
+                isReceived = false;
+            }
+        }
+
+
+
+
+    
+
+
+
+       
+
+    }
+
+   
+}
